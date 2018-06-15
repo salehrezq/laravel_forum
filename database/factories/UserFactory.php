cf185cdff408b245354079275ed031843febe42b
Saleh Rezq <salehrezq@gmail.com>
2018-06-16 01:36:15 +0200

    Intial setup

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,3 +21,19 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Thread::class, function (Faker $faker) {
+    return [
+        'user_id' => mt_rand(1, 20),
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+    ];
+});
+
+$factory->define(App\Reply::class, function (Faker $faker) {
+    return [
+        'user_id' => mt_rand(1, 20),
+        'thread_id' => mt_rand(1, 20),
+        'body' => $faker->paragraph,
+    ];
+});
