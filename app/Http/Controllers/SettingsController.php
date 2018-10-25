012d16334b128a28c19f92050299061ca61c0be9
Saleh Rezq <salehrezq@gmail.com>
2018-10-25 05:35:33 +0200

    Prevent anonymous visitors from the settings page

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -11,7 +11,7 @@ class SettingsController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth')->except('index', 'show');
+        $this->middleware('auth');
     }
 
     /**
