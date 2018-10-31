afaecf85f39dbbc2799dbbe153a34cce1c6f6503
Saleh Rezq <salehrezq@gmail.com>
2018-10-31 06:02:30 +0200

    Prepare code for email confirmation

---
+++ resources/views/emails/confirmationemail.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>Welcome</title>
+</head>
+<body>
+<h>Thanks for registering to our service</h>
+<p>You have registered with username <strong>{{$user->name}}</strong> and email <strong>{{$user->email}}</strong></p>
+</body>
+</html>
